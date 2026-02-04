83fadfa02bf746a3335a6de85fc0f2eefc0cf69c
mohammedniyafsm <mohammedniyafpc@gmail.com>
2026-02-04 21:59:25 +0530

    fix(86d1v4fq1): Fixed Error in Notification Server

--- apps/notification-ws/src/index.ts
+++ apps/notification-ws/src/index.ts
@@ -3,20 +3,20 @@ import { WebSocketServer } from "ws";
 import { connectRedisSubscriber } from "./redis/redis.js";
 import { startRedisSubscription } from "./redis/subcribe.js";
 import { handleAuth } from "./handlers/handleAuth.js";
+import { onlineUsers } from "./onlineUsers.js";
 
 const server = createServer();
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws) => {
-    ws.on('message', msg => handleAuth(ws, msg));
+  ws.on('message', msg => handleAuth(ws, msg));
 
-    ws.on("close", () => {
-        const userId = (ws as any).userId;
-        if (userId) {
-            const { onlineUsers } = require("./onlineUsers");
-            onlineUsers.delete(userId);
-        }
-    });
+  ws.on("close", () => {
+    const userId = (ws as any).userId;
+    if (userId) {
+      onlineUsers.delete(userId);
+    }
+  });
 })
 
 async function start() {
